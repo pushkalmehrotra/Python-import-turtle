034bb50825bdc9de47b1650080750b24809b84c8
pushkalmehrotra <pushkalmehrotra774@gmail.com>
2026-01-11 15:56:39 +0530

    initial.commit

--- Import.py
+++ Import.py
@@ -2,11 +2,11 @@ import turtle
 #creating canvas
 turtle. Screen(). bgcolor("Orange")
 sc = turtle.Screen()
-sc. setup(400, 300)
+sc.setup(400, 300)
 turtle.title("Welcome to Turtle Window")
-board = turtle. Turtle()
+board = turtle.Turtle()
 for i in range(4):
     board. forward(100)
     board. left (90)
     i = i+1
-Turtle.done()
+turtle.done()
